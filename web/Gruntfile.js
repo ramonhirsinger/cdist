7dc3ea683ce892d51d7520777cafb13a846328e6
Ramon Hirsinger <rhirsinger@aol.de>
2017-05-03 14:58:15 +0200

    third commit

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -1,16 +1,55 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        sass: {
-            options: {
-                sourceMap: true
+
+        pkg: grunt.file.readJSON("package.json"),
+
+        paths: {
+            src: {
+                concat: 'script/*.js',
+                scss: 'scss/style.scss'
             },
+            dest: {
+                concat: 'js/main.js',
+                concatmin: 'js/main.min.js',
+                targz: 'js/main.min.js.gz',
+                css: 'css/main.css'
+            }
+        },
+        concat: {
+            js: {
+                options: {
+                    separator: ';'
+                },
+                src: '<%= paths.src.concat %>',
+                dest: '<%= paths.dest.concat %>'
+            }
+        },
+        uglify: {
+            files: {
+                src: '<%= paths.dest.concat %>',
+                dest: '<%= paths.dest.concatmin %>'
+            }
+        },
+        compress: {
+            files: {
+                src: '<%= paths.dest.concatmin %>',
+                dest: '<%= paths.dest.targz %>'
+            }
+        },
+        sass: {
             dist: {
+                options: {
+                    style: 'compressed'
+                },
                 files: {
-                    './css/style.css': './scss/*.scss'
+                    '<%= paths.dest.css %>': '<%= paths.src.scss %>'
                 }
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['concat', 'uglify', 'compress', 'sass:dist']);
 };
